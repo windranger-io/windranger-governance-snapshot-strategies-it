f776513264044f4179afc49f53e205288fb9a863
CJ <CjHare@users.noreply.github.com>
2021-09-29 22:28:14 +1000

    feat: initialising voting power for admin

--- scripts/voting-power.ts
+++ scripts/voting-power.ts
@@ -1,18 +1,23 @@
 import {run, ethers} from 'hardhat'
 import {log} from '../config/logging'
-import {BigNumber, Contract} from 'ethers'
+import {BigNumber} from 'ethers'
 
 const erc20Abi = [
-  'function balanceOf(address account) external view returns (uint256)'
+  'function balanceOf(address account) external view returns (uint256)',
+  'function getCurrentVotes(address account) external view returns (uint256)',
+  'function balanceOfAt(address account, uint256 snapshotId) public view returns (uint256)',
+  'function snapshot() external returns (uint256)'
 ]
 const governanceAbi = [
+  'function setVotingPower() external',
   'function getVotes(address account, bytes32 role) public view returns (uint256)',
   'function setVotingPowerSingleAdmin(address voter, uint256 votingPower) external',
-  'function delegate(bytes32 role, uint256 votes, address delegatee) external'
+  'function delegate(bytes32 role, uint256 votes, address delegatee) external',
+  'function setVotingPowerSingleAdmin(address voter, uint256 votingPower)'
 ]
 
-const erc20Address = '0x959922be3caee4b8cd9a407cc3ac1c251c2007b1'
-const governanceAddress = '0x68b1d87f95878fe05b998f19b66f4baba5de1aed'
+const erc20Address = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
+const governanceAddress = '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0'
 
 const treasuryRole = ethers.utils.keccak256(
   ethers.utils.toUtf8Bytes('TREASURY_ROLE')
@@ -32,26 +37,47 @@ async function main() {
     admin
   )
 
+  // Create a snapshot or the BitDao voting power returns zero irrespective of balance
+  //  await erc20.snapshot()
+
   const balance = await erc20.balanceOf(admin.address)
+  log.info('%s has token balance of %s', admin.address, balance)
 
-  log.info('%s has balanceOf %s', admin.address, balance)
+  /*
+  const balanceOfAt = await erc20.balanceOfAt(admin.address, 1)
+  log.info(
+    '%s at snapshot zero has token balance of %s',
+    admin.address,
+    balanceOfAt
+  )
 
-  logVotes(governance, admin.address, treasuryRole)
+  const tokenVotes = await erc20.getCurrentVotes(admin.address)
+  log.info('%s has token votes of %s', admin.address, tokenVotes)
+*/
 
-  const delegatedVotes = BigNumber.from(556632)
+  const votingPower = await governance.getVotes(admin.address, treasuryRole)
+  log.info('%s role: %s, votes: %s', admin.address, treasuryRole, votingPower)
 
-  await governance.delegate(treasuryRole, delegatedVotes, admin.address)
+  const delegatedVotes = BigNumber.from(6601234567890123456780n)
 
-  logVotes(governance, admin.address, treasuryRole)
-}
+  // Relies on BitDAO current votes, which don't seem to get set
+  // await governance.setVotingPower()
 
-async function logVotes(
-  governance: Contract,
-  adminAddress: string,
-  role: string
-) {
-  const votingPower = await governance.getVotes(adminAddress, role)
-  log.info('%s role: %s, votes: %s', adminAddress, role, votingPower)
+  // Fails as there is no voting power to delegate yet
+  // await governance.delegate(treasuryRole, delegatedVotes, admin.address)
+
+  await governance.setVotingPowerSingleAdmin(admin.address, delegatedVotes)
+
+  const votingPowerAfter = await governance.getVotes(
+    admin.address,
+    treasuryRole
+  )
+  log.info(
+    '%s role: %s, votes: %s',
+    admin.address,
+    treasuryRole,
+    votingPowerAfter
+  )
 }
 
 main()
